9c775c9d960ba379b4c1bb04a1a6770ff4f294b1
karishma <88223924+karishma-dahiya@users.noreply.github.com>
2023-11-25 19:05:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let app = express();
 
 app.use(express.json());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "movieswebapp.web.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept,  authorization"
